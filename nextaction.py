2a79c8fbda3c9b8a228f303176247f09f70e5d77
gszecsenyi <szgergely@gmail.com>
2017-12-25 14:37:24 +0100

    Skip completed subtasks.

--- nextaction.py
+++ nextaction.py
@@ -28,7 +28,7 @@ def get_subitems(items, parent_item=None):
             if item['indent'] == parent_item['indent'] and item['id'] != parent_item['id']:
                 return result_items
             elif item['indent'] == required_indent and found:
-                result_items.append(item)
+                    result_items.append(item)
         elif item['indent'] == required_indent:
             result_items.append(item)
     return result_items
@@ -39,14 +39,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--api_key', help='Todoist API Key')
     parser.add_argument('-l', '--label', help='The next action label to use', default='next_action')
-    parser.add_argument('-d', '--delay', help='Specify the delay in seconds between syncs', default=5, type=int)
+    parser.add_argument('-d', '--delay', help='Specify the delay in seconds between syncs', default=10, type=int)
     parser.add_argument('--debug', help='Enable debugging', action='store_true')
     parser.add_argument('--inbox', help='The method the Inbox project should be processed',
                         default='parallel', choices=['parallel', 'serial'])
     parser.add_argument('--parallel_suffix', default='.')
     parser.add_argument('--serial_suffix', default='_')
     parser.add_argument('--hide_future', help='Hide future dated next actions until the specified number of days',
-                        default=7, type=int)
+                        default=1, type=int)
     parser.add_argument('--onetime', help='Update Todoist once and exit', action='store_true')
     args = parser.parse_args()
 
@@ -144,8 +144,11 @@ def main():
                         if item_type or len(child_items) > 0:
                             # Process serial tagged items
                             if item_type != 'parallel':
-                                for idx, child_item in enumerate(child_items):
-                                    if idx == 0:
+                                counter = 0
+                                for idx, child_item in enumerate(child_items):                                    
+                                    if child_item['checked'] == 1:
+                                        counter = counter + 1
+                                    if idx == counter:
                                         add_label(child_item, label_id)
                                     else:
                                         remove_label(child_item, label_id)
